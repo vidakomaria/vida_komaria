4a1ac70f6467bda1f45e08c066e4cfaaf1cd1c29
Vida <vidakomaria47@gmail.com>
2025-03-16 12:47:36 +0700

    Updates

---
+++ precache-manifest.b5160e7c1bb8e2e38847aa0be49e30e3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "554e1f201870120d4a3c689149a9edad",
+    "url": "/vida_komaria/index.html"
+  },
+  {
+    "revision": "f6838d79f70bcfab9f54",
+    "url": "/vida_komaria/static/css/main.855d85f2.chunk.css"
+  },
+  {
+    "revision": "ee12598ea249fd582115",
+    "url": "/vida_komaria/static/js/2.df37a3ad.chunk.js"
+  },
+  {
+    "revision": "0cb65722891933d0cc7de3183442346b",
+    "url": "/vida_komaria/static/js/2.df37a3ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "636aa98f8a53ea639595",
+    "url": "/vida_komaria/static/js/3.cbf43f5a.chunk.js"
+  },
+  {
+    "revision": "f6838d79f70bcfab9f54",
+    "url": "/vida_komaria/static/js/main.3b7f6a8c.chunk.js"
+  },
+  {
+    "revision": "da40fd9e1b7ea8486185",
+    "url": "/vida_komaria/static/js/runtime-main.995c6ea0.js"
+  },
+  {
+    "revision": "1a5475798ea3ced38b9204051ac7ca93",
+    "url": "/vida_komaria/static/media/Profile.1a547579.png"
+  },
+  {
+    "revision": "cf0131f5730cac2e2c1407fc422035ea",
+    "url": "/vida_komaria/static/media/Vida_Komaria_Curriculum_Vitae.cf0131f5.pdf"
+  }
+]);
